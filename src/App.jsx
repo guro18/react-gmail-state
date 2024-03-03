434582f5d649bd81aa47e3cf6d3f86d2b147922a
guro18 <rollgustav95@gmail.com>
2024-03-03 16:21:59 +0100

    core completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { useState } from 'react'
+
 import Header from './components/Header'
 import initialEmails from './data/emails'
 
 import './styles/App.css'
 
+
 function App() {
   // Use initialEmails for state
+  const [emails, setEmails] = useState(initialEmails)
   console.log(initialEmails)
 
   return (
@@ -28,7 +32,7 @@ function App() {
           </li>
 
           <li className="item toggle">
-            <label for="hide-read">Hide read</label>
+            <label htmlFor="hide-read">Hide read</label>
             <input
               id="hide-read"
               type="checkbox"
@@ -38,7 +42,22 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main>
+        <ul className="emails">
+          {emails.map(email => (
+            <li key={email.id} className={`email ${email.read ? 'read' : 'unread'}`}>
+              <div className="select">
+                <input className="select-checkbox" type="checkbox" />
+              </div>
+              <div className="star">
+                <input className="star-checkbox" type="checkbox" defaultChecked={email.starred} />
+              </div>
+              <div className="sender">{email.sender}</div>
+              <div className="title">{email.title}</div>
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
   )
 }
